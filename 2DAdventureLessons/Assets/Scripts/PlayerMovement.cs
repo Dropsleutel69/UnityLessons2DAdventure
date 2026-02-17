bb30b88b608d58b6d4068481c7247de5f6d44815
Dropsleutel69 <timvdheide2008@gmail.com>
2026-02-17 11:33:25 +0100

    Player input DAW done

---
+++ 2DAdventureLessons/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            
+        }
+    }
+}
